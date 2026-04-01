21c6108f55b06e747ea22fa20fd40eb24b39aaac
Deniz Köse <deniz8011@gmail.com>
2026-04-01 15:31:24 +0300

    first commit

---
+++ src/lib/menu.ts
@@ -0,0 +1,45 @@
+export const sidebarMenu = [
+    {
+        title: "Home",
+        url: "/",
+        icon: "lucide:home"
+    },
+    {
+        title: "About",
+        url: "/about",
+        icon: "lucide:user"
+    },
+    {
+        title: "Projects",
+        url: "/projects",
+        icon: "lucide:folder"
+    },
+    {
+        title: "Contact",
+        url: "/contact",
+        icon: "lucide:mail"
+    }
+];
+
+export const navbarMenu = [
+    {
+        title: "About",
+        url: "/about",
+        icon: "lucide:user"
+    },
+    {
+        title: "Projects",
+        url: "/projects",
+        icon: "lucide:folder"
+    },
+    {
+        title: "Contact",
+        url: "/contact",
+        icon: "lucide:mail"
+    },
+    {
+        title: "Github",
+        url: "https://github.com/denizkose",
+        icon: "streamline-logos:github-logo-2-solid"
+    }
+];
---
+++ src/lib/projects.ts
@@ -0,0 +1,15 @@
+interface Project {
+    title: string;
+    description: string;
+    url: string;
+    image: string;
+}
+
+export const projects: Project[] = [
+    {
+        title: "ShowMeTheKey OBS",
+        description: "A lightweight, customizable, and blazing-fast OBS Browser Source overlay for displaying keystrokes and mouse clicks.",
+        url: "https://github.com/denizkose/smtk-obs",
+        image: "smtk-obs.webp"
+    }
+];
